2f1cdc89baca20ebb918c1f90b5a7e29bb462755
hai.d <hai.d@digibird.io>
2023-04-10 20:28:34 +0700

    add file

--- dockerize/start.sh
+++ dockerize/start.sh
@@ -13,16 +13,6 @@
     && php artisan event:cache \
     && php artisan view:cache
 
-cd /var/www/website-new
-echo " Docker stop old image .."
-docker stop website-new-app || true && docker rm -fv website-new-app || true
-
-echo "Docker running app ..."
-
-docker run -d -ti -e CONTAINER_ROLE=app -e DB_CONNECTION="mysql" -e DB_HOST="mysql" -e DB_PORT="3306" -e DB_DATABASE="website_news" -e DB_USERNAME="root" -e DB_PASSWORD="password" -e  -p 5082:8000 --name=website-new-app --restart=unless-stopped website-new-app
-
-docker logs -f website-new-app
-
 echo "Done"
   echo "App is running..."
   php artisan serve --host=0.0.0.0
